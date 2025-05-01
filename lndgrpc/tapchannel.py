80322e3e1a8bfb5fb901f6744ce916a5e58f285b
ZZiigguurraatt <ZZiigguurraatt>
2025-05-01 11:11:20 -0400

    tapchannel.py: add group key support

--- lndgrpc/tapchannel.py
+++ lndgrpc/tapchannel.py
@@ -20,8 +20,8 @@ class TaprootAssetChannelsRPC(BaseClient):
         return self._tapchannel_stub
 
     @handle_rpc_errors
-    def add_taprootasset_invoice(self, asset_id, asset_amount=0, peer_pubkey=None, hodl_invoice=None, **kwargs):
-        request = tapchannel.AddInvoiceRequest(asset_id=asset_id, asset_amount=asset_amount, peer_pubkey=peer_pubkey, hodl_invoice=hodl_invoice, invoice_request=lightning.Invoice(**kwargs))
+    def add_taprootasset_invoice(self, asset_id=None,  group_key=None, asset_amount=0, peer_pubkey=None, hodl_invoice=None, **kwargs):
+        request = tapchannel.AddInvoiceRequest(asset_id=asset_id, group_key=group_key, asset_amount=asset_amount, peer_pubkey=peer_pubkey, hodl_invoice=hodl_invoice, invoice_request=lightning.Invoice(**kwargs))
         response = self.get_tapchannel_stub().AddInvoice(request)
         return response
 
@@ -38,8 +38,8 @@ class TaprootAssetChannelsRPC(BaseClient):
         return response
 
     @handle_rpc_errors
-    def send_taprootasset_payment(self, asset_id, asset_amount=0, peer_pubkey=None, **kwargs):
-        request = tapchannel.SendPaymentRequest(asset_id=asset_id, asset_amount=asset_amount, peer_pubkey=peer_pubkey, payment_request=router.SendPaymentRequest(**kwargs))
+    def send_taprootasset_payment(self, asset_id=None, group_key=None, asset_amount=0, peer_pubkey=None, **kwargs):
+        request = tapchannel.SendPaymentRequest(asset_id=asset_id, group_key=group_key, asset_amount=asset_amount, peer_pubkey=peer_pubkey, payment_request=router.SendPaymentRequest(**kwargs))
         response = self.get_tapchannel_stub().SendPayment(request)
         return response
 
